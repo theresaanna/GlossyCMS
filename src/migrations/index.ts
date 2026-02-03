86a1790c14a493e0137e826cbf7baea06f2a341f
theresa <theresasumma@gmail.com>
2026-02-03 00:48:26 -0800

    initial commit

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260202_232218 from './20260202_232218';
+
+export const migrations = [
+  {
+    up: migration_20260202_232218.up,
+    down: migration_20260202_232218.down,
+    name: '20260202_232218'
+  },
+];
